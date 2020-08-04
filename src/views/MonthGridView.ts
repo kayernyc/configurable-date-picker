d08817c85b20efa487d2d7aeea71013f86eaec55
Kathrin Ayer <kathrin.ayer@gmail.com>
2020-08-04 12:19:36 -0400

    stub out view

---
+++ src/views/MonthGridView.ts
@@ -0,0 +1,34 @@
+/**
+ * Only months in a grid
+ */
+
+import DatePickerFactory from "../models/DatePickerFactory";
+import DatePickerGridView from "./DatePickerGridView";
+import DatePickerView from "./DatePickerViewInterface";
+
+import VirtualDom from "./VirtualDom";
+
+export default class MonthGridView extends DatePickerGridView
+  implements DatePickerView {
+  continuousScroll: boolean;
+  virtualDom: VirtualDom
+
+  constructor(model: DatePickerFactory, continuousScroll = true) {
+    super(model, (continuousScroll = true));
+
+    if (this.virtualDom) {
+      // DatePickerBaseView determines if a virtualDom is needed
+      this.buildDateView = this.virtualDom.buildView.bind(this.virtualDom)
+    }
+
+    this.populateView()
+  }
+
+  populateView(
+    model: DatePickerFactory = this.model,
+    frameElement: HTMLElement = this.frameElement
+  ) {
+    const arr = model.dateArray(12);
+    this.buildDateView(arr);
+  }
+}
