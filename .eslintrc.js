bf2a386b3367c0ec7a89c091c6710f0423dec4e9
Kathrin Ayer <kathrin.ayer@gmail.com>
2020-09-12 09:38:44 -0400

    fix unused vars and disable restricted template rules
    Used vars in the error message since abstract functions are only allowed in abstract classes

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,6 +42,7 @@ module.exports = {
     },
   },
   rules: {
+    "@typescript-eslint/restrict-template-expressions": "off",
     "unicorn/prefer-node-remove": "off",
     "unicorn/prefer-spread": "off",
     "unicorn/filename-case": [
