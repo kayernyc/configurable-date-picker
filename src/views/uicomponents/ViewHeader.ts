54a63f934673a6f5f0a296aaaea9a04d2f7a6082
Kathrin Ayer <kathrin.ayer@gmail.com>
2020-09-20 08:12:22 -0400

    Add text variable.
    Added text variable and element for updated time stamp.

--- src/views/uicomponents/ViewHeader.ts
+++ src/views/uicomponents/ViewHeader.ts
@@ -1,18 +1,30 @@
 import ViewConfiguration from "../../enums/ViewConfiguration";
 
 export default class ViewHeader {
+  private mainText: string
+
   constructor(viewConfiguration: ViewConfiguration) {
-    console.log('I am here')
+    console.log('I am here');
+    this.mainText = 'bob'
   }
 
   private createHeaderElement(): HTMLElement {
     const header = document.createElement('div');
-    header.innerHTML = 'placeholer';
+    header.className = 'view-header'
+    const mainText = document.createElement('div');
+    mainText.className = 'main-text'
+    mainText.innerHTML = 'placeholer';
+    header.append(mainText)
     return header;
   }
 
+  // API
   append(parentElement: HTMLElement): void {
-    const header = this.createHeaderElement()
+    const header = this.createHeaderElement();
     parentElement.append(header);
+  }
+
+  updateMainText(text: string) {
+    console.log(text)
   }
 }
